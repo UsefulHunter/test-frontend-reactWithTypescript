760fec124624755a7a4d1c38895e6d0ee46bb50d
Brian Icaro D. Marques <bi.dm@hotmail.com>
2019-12-02 13:03:46 -0300

    build on heroku

---
+++ build/precache-manifest.3409540a77349245c359feffddb5b1bd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8f94abb75c35e05f08563ea1d12d272",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef4e0a63864eb6fcfaea",
+    "url": "/static/css/main.5357f719.chunk.css"
+  },
+  {
+    "revision": "28f966f0a52efe575109",
+    "url": "/static/js/2.2152986b.chunk.js"
+  },
+  {
+    "revision": "ef4e0a63864eb6fcfaea",
+    "url": "/static/js/main.1119b5a9.chunk.js"
+  },
+  {
+    "revision": "dff220c1d99a9b1d67c6",
+    "url": "/static/js/runtime-main.2cb846a1.js"
+  }
+]);
